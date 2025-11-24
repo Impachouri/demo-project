849ecaf1f129b60f56ddea7c1675255d9ccc2800
Aman Pachouri <ampachouri09@gmail.com>
2025-11-24 23:15:54 +0530

    basic server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,17 @@
+
+const express = require('express');
 const app = express();
+
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('<h1>Welcome to Home Page</h1>');
+});
+
+app.get('/about', (req, res) => {
+  res.send('<h1>This is the About Page</h1>');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
